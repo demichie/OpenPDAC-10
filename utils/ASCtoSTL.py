e6cc520095b26156d3937821ba1f375b55c1e9ba
Mattia de' Michieli Vitturi <demichie@gmail.com>
2022-04-09 08:53:39 +0200

    added pre-processing script
    added script to create fissure/conduit

---
+++ utils/ASCtoSTL.py
@@ -0,0 +1,392 @@
+from matplotlib import pyplot
+from shapely.geometry import LineString, Point
+from shapely.affinity import translate
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.spatial import Delaunay
+from stl import mesh
+from scipy import interpolate
+from linecache import getline
+import sys
+import os.path
+import time
+import pandas as pd
+
+DEM_file = 'demvulc2m.asc'
+
+resample = 10  # subsampling factor of the original DEM
+dist0 = 20.0  # semi-width of the fissure/radius of the cylinder
+enne = 2.0  # shape parameter (1 linear, 2 spherical)
+depth = 50.0  # depth of the fissure
+nlevels = 5  # levels of refinements of the subsampled grid
+
+xb = 0.0  # horizontal x-translation of the base of the fissure/conduit
+yb = 0.0  # horizontal y-translation of the base of the fissure/conduit
+
+xc = 496395  # x of first point of fissure/x-center of cylinder (UTM)
+yc = 4251735  # y of first point of fissure/y-center of cylinder (UTM)
+
+# FOR CYLINDRICAL FISSURE
+line = LineString([(xc, yc), (xc, yc)])
+
+# FOR LINEAR FISSURE
+# line = LineString([(xc, yc), (xc+20, yc+10), (xc+40, yc+15)])
+
+
+# Print iterations progress
+def printProgressBar(iteration,
+                     total,
+                     prefix='',
+                     suffix='',
+                     decimals=1,
+                     bar_length=100):
+    """
+    Call in a loop to create terminal progress bar
+
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        bar_length  - Optional  : character length of bar (Int)
+    """
+    str_format = "{0:." + str(decimals) + "f}"
+    percents = str_format.format(100 * (iteration / float(total)))
+    filled_length = int(round(bar_length * iteration / float(total)))
+    bar = '█' * filled_length + '-' * (bar_length - filled_length)
+
+    sys.stdout.write('\r%s |%s| %s%s %s' %
+                     (prefix, bar, percents, '%', suffix)),
+
+    if iteration == total:
+        sys.stdout.write('\n')
+    sys.stdout.flush()
+
+
+print('Reading DEM file: ' + DEM_file)
+# Parse the topography header
+hdr = [getline(DEM_file, i) for i in range(1, 7)]
+values = [float(h.split()[-1].strip()) for h in hdr]
+cols, rows, lx, ly, cell, nd = values
+cols = int(cols)
+rows = int(rows)
+
+xs_DEM = lx + 0.5 * cell + np.linspace(0, (cols - 1) * cell, cols)
+ys_DEM = ly + 0.5 * cell + np.linspace(0, (rows - 1) * cell, rows)
+
+# Load the topography into a numpy array
+DEM = pd.read_table(DEM_file, delim_whitespace=True, header=None,
+                    skiprows=6).astype(float).values
+DEM = np.flipud(DEM)
+DEM[DEM == nd] = 0.0
+
+xinit = np.linspace(0, (cols - 1) * cell, cols)
+yinit = np.linspace(0, (rows - 1) * cell, rows)
+
+Xinit, Yinit = np.meshgrid(xinit, yinit)
+Zinit = DEM
+
+# translate the linestring (relative reference system with (lx,ly)=(0,0))
+line = translate(line, -lx, -ly)
+
+bb = line.bounds
+
+# interpolate with values from original fine grid
+f = interpolate.interp2d(xinit, yinit, Zinit, kind='linear')
+
+# coarsening of the original grid
+xinit = xinit[::resample]
+yinit = yinit[::resample]
+
+Xinit = Xinit[::resample, ::resample]
+Yinit = Yinit[::resample, ::resample]
+Zinit = Zinit[::resample, ::resample]
+
+# list of points of DEM for the refined nested grid
+x_check = []
+y_check = []
+z_check = []
+
+# list of displacements
+dx = []
+dy = []
+dz = []
+
+# boundaing box of first refinement level
+xmin_bb = bb[0] - nlevels * dist0
+ymin_bb = bb[1] - nlevels * dist0
+xmax_bb = bb[2] + nlevels * dist0
+ymax_bb = bb[3] + nlevels * dist0
+
+# x bounding box indexes
+idx_min = np.searchsorted(xinit, xmin_bb, side="left") - 1
+idx_max = np.searchsorted(xinit, xmax_bb, side="left")
+
+x0 = xinit[idx_min:idx_max + 1]
+
+# y bounding box indexes
+idy_min = np.searchsorted(yinit, ymin_bb, side="left") - 1
+idy_max = np.searchsorted(yinit, ymax_bb, side="left")
+
+y0 = yinit[idy_min:idy_max + 1]
+
+# add original points on the west of first bounding box
+x_check.extend(Xinit[:, 0:idx_min].ravel())
+y_check.extend(Yinit[:, 0:idx_min].ravel())
+z_check.extend(Zinit[:, 0:idx_min].ravel())
+
+dx.extend(0.0 * Xinit[:, 0:idx_min].ravel())
+dy.extend(0.0 * Xinit[:, 0:idx_min].ravel())
+dz.extend(0.0 * Xinit[:, 0:idx_min].ravel())
+
+# add original points on the north of first bounding box
+x_check.extend(Xinit[0:idy_min, idx_min:idx_max + 1].ravel())
+y_check.extend(Yinit[0:idy_min, idx_min:idx_max + 1].ravel())
+z_check.extend(Zinit[0:idy_min, idx_min:idx_max + 1].ravel())
+
+dx.extend(0.0 * Xinit[0:idy_min, idx_min:idx_max + 1].ravel())
+dy.extend(0.0 * Xinit[0:idy_min, idx_min:idx_max + 1].ravel())
+dz.extend(0.0 * Xinit[0:idy_min, idx_min:idx_max + 1].ravel())
+
+# add original points on the south of first bounding box
+x_check.extend(Xinit[idy_max + 1:, idx_min:idx_max + 1].ravel())
+y_check.extend(Yinit[idy_max + 1:, idx_min:idx_max + 1].ravel())
+z_check.extend(Zinit[idy_max + 1:, idx_min:idx_max + 1].ravel())
+
+dx.extend(0.0 * Xinit[idy_max + 1:, idx_min:idx_max + 1].ravel())
+dy.extend(0.0 * Xinit[idy_max + 1:, idx_min:idx_max + 1].ravel())
+dz.extend(0.0 * Xinit[idy_max + 1:, idx_min:idx_max + 1].ravel())
+
+# add original points on the east of first bounding box
+x_check.extend(Xinit[:, idx_max + 1:].ravel())
+y_check.extend(Yinit[:, idx_max + 1:].ravel())
+z_check.extend(Zinit[:, idx_max + 1:].ravel())
+
+dx.extend(0.0 * Xinit[:, idx_max + 1:].ravel())
+dy.extend(0.0 * Xinit[:, idx_max + 1:].ravel())
+dz.extend(0.0 * Xinit[:, idx_max + 1:].ravel())
+
+dist_lev0 = 1.e10
+
+# loop over revinement levels
+for ilevel in range(nlevels):
+
+    print('')
+    print('Refinement level:', ilevel)
+
+    X0, Y0 = np.meshgrid(x0, y0)
+
+    X0_1d = X0.ravel()
+    Y0_1d = Y0.ravel()
+
+    nxy = len(X0_1d)
+
+    dist_lev = dist0 * (nlevels - ilevel)
+    print('Distance range:', dist_lev0, dist_lev)
+    print('Points to check:', nxy)
+
+    i = 0
+
+    for j, (x, y) in enumerate(zip(X0_1d, Y0_1d)):
+
+        printProgressBar(np.asarray(i) + 1,
+                         nxy,
+                         prefix='Progress:',
+                         suffix='Complete',
+                         decimals=1,
+                         bar_length=50)
+
+        dist = line.distance(Point(x, y))
+
+        if (dist > dist_lev) and (dist <= dist_lev0):
+
+            dx.append(0.0)
+            dy.append(0.0)
+            dz.append(0.0)
+
+            x_check.append(x)
+            y_check.append(y)
+
+            z = f(x, y)
+
+            z_check.append(float(z))
+
+        i += 1
+
+    # bounding box of new refinement level
+    xmin_bb = bb[0] - dist_lev
+    ymin_bb = bb[1] - dist_lev
+    xmax_bb = bb[2] + dist_lev
+    ymax_bb = bb[3] + dist_lev
+
+    # print('xmin_bb,xmax_bb',xmin_bb,xmax_bb)
+    # print('ymin_bb,ymax_bb',ymin_bb,ymax_bb)
+
+    idx_min = np.searchsorted(x0, xmin_bb, side="left") - 1
+    idx_max = np.searchsorted(x0, xmax_bb, side="left")
+    nx = 2 * (idx_max - idx_min) + 1
+
+    x1 = np.linspace(x0[idx_min], x0[idx_max], nx)
+
+    idx_min = np.searchsorted(y0, ymin_bb, side="left") - 1
+    idx_max = np.searchsorted(y0, ymax_bb, side="left")
+    ny = 2 * (idx_max - idx_min) + 1
+    y1 = np.linspace(y0[idx_min], y0[idx_max], ny)
+
+    x0 = x1
+    y0 = y1
+    dist_lev0 = dist_lev
+
+print('')
+print('Refinement level:', nlevels)
+
+X1, Y1 = np.meshgrid(x1, y1)
+
+X1_1d = X1.ravel()
+Y1_1d = Y1.ravel()
+
+nxy = len(X1_1d)
+print('Distance range:', dist_lev0, 0)
+print('Points to check:', nxy)
+
+n_out = len(x_check)
+
+i = 0
+
+for j, (x, y) in enumerate(zip(X1_1d, Y1_1d)):
+
+    printProgressBar(np.asarray(i) + 1,
+                     nxy,
+                     prefix='Progress:',
+                     suffix='Complete',
+                     decimals=1,
+                     bar_length=50)
+    dist = line.distance(Point(x, y))
+
+    if (dist <= dist_lev):
+
+        dz_rel = -(1.0 - (dist / dist0)**enne)**(1.0 / enne)
+
+        dx.append(dz_rel * xb)
+        dy.append(dz_rel * yb)
+        dz.append(depth * dz_rel)
+
+        z = f(x, y)
+
+        x_check.append(x)
+        y_check.append(y)
+        z_check.append(float(z))
+
+    i += 1
+
+x_old = np.array(x_check)
+y_old = np.array(y_check)
+z_old = np.array(z_check)
+
+x_new = np.array(x_check) + np.array(dx)
+y_new = np.array(y_check) + np.array(dy)
+z_new = np.array(z_check) + np.array(dz)
+
+points = []
+
+for i, (x, y) in enumerate(zip(x_check, y_check)):
+
+    points.append([x, y])
+
+points = np.asarray(points)
+
+print('')
+print('Building Delaunay triangulation')
+
+tri = Delaunay(points)
+
+print('Delaunay triangulation completed')
+
+# split the triangulation
+inner_tri_list = []
+
+tri_simpl = tri.simplices.copy()
+
+vert = range(n_out, len(x_check) + 1)
+
+inner_tri_list = np.in1d(tri_simpl[:, 0], vert)
+inner_tri_list = np.logical_or(inner_tri_list, np.in1d(tri_simpl[:, 1], vert))
+inner_tri_list = np.logical_or(inner_tri_list, np.in1d(tri_simpl[:, 2], vert))
+
+inner_tri_list = np.arange(tri_simpl.shape[0])[inner_tri_list]
+
+tri_in = tri_simpl[inner_tri_list, :]
+
+outer_tri_list = list(set(range(tri_simpl.shape[0])) - set(inner_tri_list))
+
+tri_out = tri_simpl[outer_tri_list, :]
+
+"""
+fig = plt.figure()
+plt.triplot(points[:,0], points[:,1], tri.simplices)
+plt.plot(points[:,0], points[:,1], 'o')
+
+x, y = line.xy
+plt.plot(x, y, 'k')
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+ax.scatter(x_new,y_new,z_new)
+"""
+
+# original elevation 3D point refined grid
+vertices_old = np.column_stack((x_old, y_old, z_old))
+
+# modified elevation 3D point refined grid
+vertices = np.column_stack((x_new, y_new, z_new))
+
+# Create the full mesh
+print('')
+print('Saving full stl')
+faces = tri.simplices
+
+surface = mesh.Mesh(np.zeros(faces.shape[0], dtype=mesh.Mesh.dtype))
+for i, f in enumerate(faces):
+    for j in range(3):
+        surface.vectors[i][j] = vertices[f[j], :]
+
+surface.save('surface.stl')
+
+# Create the inside mesh
+print('Saving in stl')
+
+faces = tri_in
+
+surface = mesh.Mesh(np.zeros(faces.shape[0], dtype=mesh.Mesh.dtype))
+for i, f in enumerate(faces):
+    for j in range(3):
+        surface.vectors[i][j] = vertices[f[j], :]
+
+surface.save('surface_in.stl')
+
+# Create the outside mesh
+print('Saving out stl')
+faces = tri_out
+
+surface = mesh.Mesh(np.zeros(faces.shape[0], dtype=mesh.Mesh.dtype))
+for i, f in enumerate(faces):
+    for j in range(3):
+        surface.vectors[i][j] = vertices[f[j], :]
+
+surface.save('surface_out.stl')
+
+# Create the inside mesh closed on top
+print('Saving closed in stl')
+
+faces = tri_in
+
+surface = mesh.Mesh(np.zeros(2 * faces.shape[0], dtype=mesh.Mesh.dtype))
+for i, f in enumerate(faces):
+    for j in range(3):
+        surface.vectors[i][j] = vertices[f[j], :]
+        surface.vectors[i + faces.shape[0]][j] = vertices_old[f[j], :]
+
+surface.save('surface_in_closed.stl')
+
+# plt.show()
